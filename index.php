e334056f0a84f95bbc4dfc8c57ced8dc68cdb517
Maxim Zhukotsky <m.zhukotsky@gmail.com>
2016-12-06 20:50:41 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Viedoeditor</title>
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/dropzone.css">
+
+
+</head>
+<body>
+  <form action="upload.php"  class="dropzone">
+    <div class="fallback">
+      <input name="file" type="file" multiple />
+    </div>
+  </form>
+
+</body>
+
+<!-- Sripts -->
+
+<script src="js/dropzone.js"></script>
+<script src="js/jquery-3.1.1.min.js"></script>
+</html>
---
+++ upload.php
@@ -0,0 +1,24 @@
+<?php
+$ds          = DIRECTORY_SEPARATOR;  //1
+
+$storeFolder = 'uploads';   //2
+
+if (!empty($_FILES)) {
+
+    $tempFile = $_FILES['file']['tmp_name'];          //3
+
+    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
+
+    $targetFile =  $targetPath. $_FILES['file']['name'];  //5
+
+    move_uploaded_file($tempFile,$targetFile); //6
+
+}
+
+
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+?>
